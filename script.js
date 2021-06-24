1510739c404929bf6364a8c8befc379fd424f8be
Andrea <rakosfalvyandrea@gmail.com>
2021-06-24 15:12:43 +0200

    found solution

--- script.js
+++ script.js
@@ -16,19 +16,36 @@ const resultArray = [];
 //     console.log(resultArray)
 // }
 
-for (let string of originalArray) {
-    for (let filter of filterArray) {
-        console.log(string, filter)
-        if (string === filter) {
-            console.log("in if")
-           
-            {break}
-        } else {
-            resultArray.push(string);
-        }
-        //{break}
-    }
+// for (let string of originalArray) {
+//     for (let filter of filterArray) {
+//         console.log(string, filter)
+//         if (string === filter) {
+//             console.log("in if")       
+//             {break}
+//         } else {
+//             resultArray.push(string);
+//         }
+//         //{break}
+//     }
+//     console.log(resultArray)
+// }
 
-    console.log(resultArray)
+// SOLUTION
+
+for (let i = 0; i < originalArray.length; i++) {
+    for (let filter of filterArray) {
+        if (originalArray[i] === filter) {
+            console.log("in if")          
+            originalArray[i] = "remove"
+        } 
+    }
+    console.log(originalArray)
 }
 
+for (let string of originalArray) {
+    if (string != "remove") {
+        resultArray.push(string);
+    }
+}
+
+console.log(resultArray)
